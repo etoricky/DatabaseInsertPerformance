d7b2cc36fa183f74df46fb2aca9a03bfdcfe307c
Ricky <etoricky@gmail.com>
2018-09-04 13:43:21 +0800

    update mongo

--- 01Inserts/MongoInsert.py
+++ 01Inserts/MongoInsert.py
@@ -8,11 +8,16 @@ with Timer() as t:
     with open('input.ssv', 'r') as infile:
         lines = infile.read().splitlines()
         t.setSize(len(lines))
+        documents = []
         for line in lines:
-            document = {
+            documents.append({
                     "time": time.time(),
                     "symbol": line[0:6],
                     "bid": float(line[7:14]),
                     "ask": float(line[15:])
-                }
-            collect.insert_one(document)
+                })
+            if len(documents)>128:
+                collect.insert_many(documents)
+                documents = []
+        collect.insert_many(documents)
+        documents = []
